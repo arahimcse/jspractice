c92a760e038af0dd635916ad7560c8dc4f1ce2c2
Abdur Rahim <arahimcse@gmail.com>
2023-07-09 22:27:56 +0600

    JavaScript callBack ex 2

--- js_callback/script.js
+++ js_callback/script.js
@@ -2,16 +2,44 @@
  * callBack  is a function passed as an argument to another function.
  */
 
-function disPlay(something)
+// function disPlay(something)
+// {
+//     document.getElementById("demo").innerHTML = something;
+// }
+
+
+// function higherOrder(name, className, callBack)
+// {
+//     const details = `My name is ${name} and I read in class ${className}`;
+//     callBack(details);
+// }
+
+// higherOrder("Abdur Rahim", 16, disPlay);
+
+
+
+//write a function that will return positive values in a array
+
+
+//declare a array an assign static value
+const myNumbers = [4,1,-10,-9,-6,5,9]
+
+//find positive numbers
+const positiveNumbers = negativeNumbers(myNumbers, (x) => x>=0);
+
+//display result into html tag
+document.getElementById("demo").innerHTML = positiveNumbers;
+
+//define the higher order function
+function negativeNumbers(x, callBack)
 {
-    document.getElementById("demo").innerHTML = something;
+    const myArr =[];
+    for(let arg of x)
+    {
+        if(callBack(arg))
+        {
+            myArr.push(arg);
+        }
+    }
+    return myArr;
 }
-
-
-function higherOrder(name, className, callBack)
-{
-    const details = `My name is ${name} and I read in class ${className}`;
-    callBack(details);
-}
-
-higherOrder("Abdur Rahim", 16, disPlay);
