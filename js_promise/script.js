8af74c6c1a0d7755debce055dd98cefce831f8c0
Abdur Rahim <arahimcse@gmail.com>
2023-07-12 23:28:10 +0600

    JavaScript Promise

--- js_promise/script.js
+++ js_promise/script.js
@@ -117,16 +117,157 @@ Differences aside, asynchronous and synchronous methods both offer advantages, b
  * JavaScript code running processed
  * 1. JavaScript code conver into machine code that understand by JavaScript engine. [Engine]
  * 2. Browser has call Stack, console, Web APIs, call queue/ Task Queue
+ * 
+ * 
+ * ####Output####
+ * Take order for Customer 1
+ * processing order for customer 1
+ * cooking completed (continue till 3 second)
+ * processed order for customer 1
+ * Task completed
+ * 
  */
 
-function processingOrder()
-{
-    console.log("processing order for customer 1");
-    const currentDate = new Date().getTime();
-    while(currentDate + 8000 >= new Date().getTime()); //block the application for 8 seconds. async await resolve the problem
+// function processingOrder()
+// {
+//     console.log("processing order for customer 1");
+//     const currentDate = new Date().getTime();
+//     while(currentDate + 3000 >= new Date().getTime())
+//     {
+//         console.log("cooking completed");
+//     } //block the application for 8 seconds and user cann't perform any operation in the webpages. async await resolve the problem
 
-    console.log('processed order for customer 1')
-}
-console.log('Take order for Customer 1');
-processingOrder();
-console.log("Completer Order for customer 1")
+//     console.log('processed order for customer 1')
+// }
+// console.log('Take order for Customer 1')
+// processingOrder()
+// console.log("Task completed")
+
+
+/**
+ * Example for used asynchronous function. Asynchronous function normally take multi-thread, Means when an operation/function take sometime for execution. Asynchronous function not for completion the function and switch to next line. And asynchronous function go to in WEB API's and wait for assignment time then got callback queue/Tasks Queue
+ * 
+ * ####Output#####
+ * Take an order for customer 1
+ * Processing Order for customer 1
+ * processed order
+ * Task Complete
+ * cooking complete
+ */
+// const orderProcess = () =>
+// {
+//     console.log("Processing Order for customer 1")
+//     setTimeout(() => console.log("cooking complete"), 3000)
+//     console.log('processed order')
+// }
+// console.log("Take an order for customer 1")
+// orderProcess();
+// console.log('Task Complete')
+
+/**
+ * example the above code maintained a flow for the perticular operation
+ * ####Output#######
+ * 
+ * Take an order for customer 1
+ * Processing order for customer 1
+ * Cooking completed
+ * Procssed Order of customer 1
+ * Complete order for customer 1
+ * 
+ */
+
+// const takeOrder = (customer, callBack) =>
+// {
+//     console.log(`Take an order for ${customer}`)
+//     callBack(customer)
+// }
+
+// const processOrder = (customer, callBack) =>
+// {
+//     console.log(`Processing order for ${customer}`)
+//     setTimeout(()=>{
+//         console.log("Cooking completed")
+//         console.log(`Procssed Order of ${customer}`)
+//         callBack(customer)
+//     },3000)
+// }
+
+// const completerOrder =(customer) =>{
+//     console.log(`Complete order for ${customer}`)
+// }
+
+// takeOrder('customer 1', (x) =>{ /**The callback control the flow of our task  */
+//     processOrder(x, (x)=>{
+//         completerOrder(x)
+//     })
+// })
+
+/**
+ * ###Output###
+ * Take an order for customer 1
+ * Processing order for customer 1
+ * Hello World
+ * Cooking completed
+ * Procssed Order of customer 1
+ * Complete order for customer 1
+ * 
+ * There have only one async function "setTimeout". There have 3 minutes delay but Javascript not stop the. JavaScript is a single thread engine, so it hand over the async function to the runtime environment (web apis, call back queue, even loop) and jump to the next statemt ment. When JavaScript engine 'call stack' free then even loop run and forward the  async function the call stack again for execution. 
+ * 
+ */
+
+// const takeOrder = (customer, callBack) =>
+// {
+//     console.log(`Take an order for ${customer}`)
+//     callBack(customer)
+// }
+
+// const processOrder = (customer, callBack) =>
+// {
+//     console.log(`Processing order for ${customer}`)
+//     setTimeout(()=>{
+//         console.log("Cooking completed")
+//         console.log(`Procssed Order of ${customer}`)
+//         callBack(customer)
+//     },3000)
+// }
+
+// const completerOrder =(customer) =>{
+//     console.log(`Complete order for ${customer}`)
+// }
+
+// takeOrder('customer 1', (x) =>{ /**The callback control the flow of our task  */
+//     processOrder(x, (x)=>{
+//         completerOrder(x)
+//     })
+// })
+
+// console.log("Hello World")
+
+
+
+/**
+ * Create promise and used promise
+ */
+
+const hasMeeting = false;
+
+const myPromise = new Promise((myResolve, myReject) =>{
+    if(!hasMeeting)
+    {
+        const meeting = {
+            name:"Technical Training Decision",
+            location:"123 Maridien Hotel",
+            time:"10:00AM"
+        }
+        myResolve(meeting)
+    } else 
+    {
+        myReject(new Error("The meeting already scheduled"))
+    }
+})
+
+//Generate promise 
+
+myPromise
+.then((value)=>{console.log(value)})
+.catch((error)=>{console.log(error)})
