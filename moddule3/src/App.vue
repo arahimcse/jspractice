db7de9df695e8200a593e8f838d5235c4cb2e766
Abdur Rahim <arahimcse@gmail.com>
2023-07-13 23:27:28 +0600

    Vue composed component with Option API

--- moddule3/src/App.vue
+++ moddule3/src/App.vue
@@ -1,4 +1,21 @@
-<script setup>
+<!-- Vue composed component
+example1. Option API
+-->
+
+<script>
+export default {
+  data(){
+    return{
+      message: "Welcome to my mastering vue.js 3 app",
+      date(){ return "<strong>13th july, 2023</strong>"},
+      person:{fname:"Abdur",lname:"Rahim",getName(){return this.fname + " " +this.lname}}
+    }
+  }
+}
+</script>
+
+
+<!-- <script setup>
 const message = 'Welcome to my mastering vue.js 3'
 const date = () => "<strong>13th july, 2023</strong>"
 const person ={
@@ -9,7 +26,7 @@ const person ={
     return this.fname + " " + this.lname;
   }
 }
-</script>
+</script> -->
 
 <template>
   <h1>{{ message }}</h1>
