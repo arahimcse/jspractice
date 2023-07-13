69eb9064eb88dc34869bef7c54545ebfba11fb0e
Abdur Rahim <arahimcse@gmail.com>
2023-07-13 23:38:26 +0600

    vue composed component [Composition API]

--- moddule3/src/App.vue
+++ moddule3/src/App.vue
@@ -1,19 +1,39 @@
-<!-- Vue composed component
-example1. Option API
--->
-
 <script>
+/**
+ * Vue composed component
+ * example1. Option API
+ * 
+ */
+//  export default {
+//   data(){
+//     return{
+//       message: "Welcome to my mastering vue.js 3 app",
+//       date(){ return "<strong>13th july, 2023</strong>"},
+//       person:{fname:"Abdur",lname:"Rahim",getName(){return this.fname + " " +this.lname}}
+//     }
+//   }
+// }
+
+/**
+ * Example : Composition API
+ */
+
 export default {
-  data(){
-    return{
-      message: "Welcome to my mastering vue.js 3 app",
-      date(){ return "<strong>13th july, 2023</strong>"},
-      person:{fname:"Abdur",lname:"Rahim",getName(){return this.fname + " " +this.lname}}
+  setup()
+  {
+    const message = "Welcome to my mastering vue.js api"
+    const date = () => "<strong>13th july, 2023</strong>"
+    const person = {
+      fanme:"Abdur",
+      lname:"Rahim",
+      getName(){
+        return this.fanme + " " + this.lname
+      }
     }
+    return {message, date, person}
   }
 }
 </script>
-
 
 <!-- <script setup>
 const message = 'Welcome to my mastering vue.js 3'
