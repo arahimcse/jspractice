415c929e562153d14af99ef33a72b62a953534b1
Abdur Rahim <arahimcse@gmail.com>
2023-07-14 07:43:33 +0600

    add bootstrap and include bootstrap very basic template

--- moddule3/src/App.vue
+++ moddule3/src/App.vue
@@ -1,4 +1,4 @@
-<script>
+<script setup>
 /**
  * Vue composed component
  * example1. Option API
@@ -18,41 +18,53 @@
  * Example : Composition API
  */
 
-export default {
-  setup()
-  {
-    const message = "Welcome to my mastering vue.js api"
-    const date = () => "<strong>13th july, 2023</strong>"
-    const person = {
-      fanme:"Abdur",
-      lname:"Rahim",
-      getName(){
-        return this.fanme + " " + this.lname
-      }
-    }
-    return {message, date, person}
-  }
-}
+// export default {
+//   setup()
+//   {
+//     const message = "Welcome to my mastering vue.js api"
+//     const date = () => "<strong>13th july, 2023</strong>"
+//     const person = {
+//       fanme:"Abdur",
+//       lname:"Rahim",
+//       getName(){
+//         return this.fanme + " " + this.lname
+//       }
+//     }
+//     return {message, date, person}
+//   }
+// }
+
+/**
+ * Example Script Setup
+ * 
+ *
+ */
+ 
+
+ import headerPart from './components/common/headerPart.vue'
+ import footerPart from './components/common/footerPart.vue'
+ import mainContent from './components/mainContent.vue'
+
+// const message = 'Welcome to my mastering vue.js 3'
+// const date = () => "<strong>13th july, 2023</strong>"
+// const person ={
+//   fname:"Abdur",
+//   lname:"Rahim",
+//   getName()
+//   {
+//     return this.fname + " " + this.lname;
+//   }
+// }
+
 </script>
 
-<!-- <script setup>
-const message = 'Welcome to my mastering vue.js 3'
-const date = () => "<strong>13th july, 2023</strong>"
-const person ={
-  fname:"Abdur",
-  lname:"Rahim",
-  getName()
-  {
-    return this.fname + " " + this.lname;
-  }
-}
-</script> -->
-
 <template>
-  <h1>{{ message }}</h1>
+  <headerPart/>
+  <!-- <h1>{{ message }}</h1>
   <p>Today is <span v-html="date()"></span></p>
-  <h3>name of the persone <span>{{ person.getName() }}</span></h3>
-  
+  <h3>name of the persone <span>{{ person.getName() }}</span></h3> -->
+  <mainContent/>
+  <footerPart/>
 </template>
 
 <style scoped>
---
+++ moddule3/src/components/common/footerPart.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <div class="mt-5 p-4 bg-dark text-white text-center">
+  <p>Footer</p>
+</div>
+
+</template>
+
+  <style scoped>
+
+</style>
